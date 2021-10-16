0165c08147ae48c37aafec95e3faf7c7215e8230
kushagra <75723079+kushagra487@users.noreply.github.com>
2021-10-17 01:12:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let ulTasks = $('#ulTasks')
+let btnAdd = $('#btnAdd')
+let btnClear = $('#btnClear')
+let inpNewTask = $('#inpNewTask')
+
+btnAdd.click(() => {
+    let listItem = $('<li>', {
+        'class': 'list-group-item',
+        text: inpNewTask.val()
+    })
+
+    listItem.click(() => {
+    listItem.toggleClass('disabled')
+    })
+
+    ulTasks.append(listItem)
+    inpNewTask.val('')
+})
+
+btnClear.click(() => inpNewTask.val(''))
